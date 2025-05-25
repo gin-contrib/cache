e8ba66101dedbecf51f1454d87a75a84a5d205c3
appleboy <appleboy.tw@gmail.com>
2025-05-25 20:55:49 +0800

    feat: embed and serve dynamic HTML templates with Gin
    - Add embedding for an HTML template using Go's embed.FS
    - Parse and set the embedded template for Gin HTML rendering
    - Introduce a new /html route serving cached HTML with dynamic data
    - Extend the HTML template with head, title, and timestamp fields for improved structure and content
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- _example/example.go
+++ _example/example.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"html/template"
 	"time"
 
 	"github.com/gin-contrib/cache"
@@ -9,8 +11,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed template.html
+var tmplFS embed.FS
+
 func main() {
 	r := gin.Default()
+
+	// Parse embedded template
+	tmpl, err := template.ParseFS(tmplFS, "template.html")
+	if err != nil {
+		panic(err)
+	}
+	r.SetHTMLTemplate(tmpl)
 
 	store := persistence.NewInMemoryStore(60 * time.Second)
 	// Non-cached Page
@@ -21,6 +33,16 @@ func main() {
 	// Cached Page
 	r.GET("/cache_ping", cache.CachePage(store, time.Minute, func(c *gin.Context) {
 		c.String(200, "pong "+fmt.Sprint(time.Now().Unix()))
+	}))
+
+	// HTML Page using embedded template
+	r.GET("/html", cache.CachePage(store, time.Minute, func(c *gin.Context) {
+		name := c.DefaultQuery("name", "guest")
+		c.HTML(200, "template.html", gin.H{
+			"title":     "Cached HTML Page",
+			"values":    "Hello from embed (" + name + ")!",
+			"timestamp": time.Now().Format(time.RFC3339),
+		})
 	}))
 
 	// Listen and Server in 0.0.0.0:8080
